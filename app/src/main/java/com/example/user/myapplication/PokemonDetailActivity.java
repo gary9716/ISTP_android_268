b9b8890c7a1b49be1ba15256065ac7083d4439f7
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-01 19:30:17 +0800

    inflate menu and add item selected listener

--- app/src/main/java/com/example/user/myapplication/PokemonDetailActivity.java
+++ app/src/main/java/com/example/user/myapplication/PokemonDetailActivity.java
@@ -4,6 +4,8 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -95,6 +97,22 @@ public class PokemonDetailActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.pokemon_detail_action_bar_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.action_save) {
+            
+        }
+        else if(itemId == R.id.action_level_up) {
 
+        }
+
+        return false;
+    }
 }
