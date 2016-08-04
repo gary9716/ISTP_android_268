8406dd45deffeded11bec6aed5d2a6e669120213
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-04 21:33:17 +0800

    add Creating menu logic and onActivityResult

--- app/src/main/java/com/example/user/myapplication/fragment/PokemonListFragment.java
+++ app/src/main/java/com/example/user/myapplication/fragment/PokemonListFragment.java
@@ -7,7 +7,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -104,4 +108,47 @@ public class PokemonListFragment extends Fragment implements AdapterView.OnItemC
         intent.putExtra(PokemonInfo.parcelKey, pokemonInfo);
         startActivityForResult(intent, detailActivityRequestCode);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == detailActivityRequestCode) {
+            if(resultCode == removeFromList) {
+                String pokemonName = data.getStringExtra(PokemonInfo.nameKey);
+                PokemonInfo pokemonInfo = adapter.getItemWithName(pokemonName);
+                if(pokemonInfo != null) {
+                    adapter.remove(pokemonInfo);
+                    Toast.makeText(activity, pokemonInfo.name + "已被存入電腦", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.list_action_bar_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.action_delete) {
+            Log.d("menuItem", "action_delete");
+            if(adapter.selectedPokemons.size() > 0)
+                alertDialog.show();
+            return true;
+        }
+        else if(itemId == R.id.action_heal) {
+            Log.d("menuItem", "action_heal");
+            return true;
+        }
+        else if(itemId == R.id.action_settings) {
+            Log.d("menuItem", "action_settings");
+            return true;
+        }
+
+        return false;
+    }
+
 }
