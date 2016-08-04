4b901d115af869a9b7f0b90dcea4b6f4d1bb8f51
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-04 21:40:44 +0800

    add fragment usage and finish onDrawerItemClickListener

--- app/src/main/java/com/example/user/myapplication/DrawerActivity.java
+++ app/src/main/java/com/example/user/myapplication/DrawerActivity.java
@@ -1,11 +1,16 @@
 package com.example.user.myapplication;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.user.myapplication.fragment.PokemonListFragment;
+import com.example.user.myapplication.fragment.TestFragment;
 import com.example.user.myapplication.model.Utils;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
@@ -24,11 +29,23 @@ public class DrawerActivity extends AppCompatActivity{
     AccountHeader headerResult;
     IProfile profile;
     Drawer drawer;
+    Fragment[] fragments;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+
+        fragments = new Fragment[3];
+        fragments[0] = PokemonListFragment.newInstance();
+        fragments[1] = TestFragment.newInstance("fake 1");
+        fragments[2] = TestFragment.newInstance("fake 2");
+
+        fragmentManager = getFragmentManager();
+        for(int i = fragments.length - 1;i >= 0;i--) {
+            replaceWithFragment(fragments[i]);
+        }
 
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -47,8 +64,7 @@ public class DrawerActivity extends AppCompatActivity{
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-
-
+                        replaceWithFragment(fragments[position - 1]);
                         return false;
                     }
                 })
@@ -67,6 +83,13 @@ public class DrawerActivity extends AppCompatActivity{
                 .withSavedInstance(savedInstanceState)
                 .build();
 
+    }
+
+    private void replaceWithFragment(Fragment fragment) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragmentContainer, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
