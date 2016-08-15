f8309c3c884c0845ec4f71d3770451212294eb22
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-08-15 21:12:04 +0800

    add ImageLoader and finish it initialization

--- app/src/main/java/com/example/user/myapplication/MyApplication.java
+++ app/src/main/java/com/example/user/myapplication/MyApplication.java
@@ -4,8 +4,12 @@ import android.app.Application;
 
 import com.example.user.myapplication.model.PokemonInfo;
 import com.example.user.myapplication.model.PokemonType;
+import com.example.user.myapplication.model.SearchPokemonInfo;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
@@ -22,12 +26,26 @@ public class MyApplication extends Application {
 
         ParseObject.registerSubclass(PokemonInfo.class);
         ParseObject.registerSubclass(PokemonType.class);
+        ParseObject.registerSubclass(SearchPokemonInfo.class);
         Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
                 .enableLocalDataStore()
                 .applicationId("d41d8cd98f00b204e9800998ecf8427e") //change to your AppId
                         //.clientKey("YJy27NUjuLfJaicKAFReic3gpCFxdemFsPrsQj05") //change to your clientKey
                 .server("http://140.112.30.43:1337/parse")
                 .build());
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+                .defaultDisplayImageOptions(defaultOptions)
+                .diskCacheSize(50 * 1024 * 1024) //50MB
+                .diskCacheFileCount(100)
+                .build();
+
+        ImageLoader.getInstance().init(config);
 
 
     }
